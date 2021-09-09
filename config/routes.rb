dd37f402e564b7442a46dc9f8dfcfc0df0310c49
yuji91 <yuji91.1227@gmail.com>
2021-09-10 01:50:08 +0900

    Remove useless routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'user_sessions/new'
-  get 'user_sessions/create'
-  get 'user_sessions/destroy'
-  get 'users/new'
-  get 'users/create'
   get 'top_pages/index'
   get 'top_pages/help'
   get    '/signup',  to: 'users#new'
