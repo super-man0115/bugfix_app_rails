5e5fffd254129426b66df71a01fb56461ebd79cb
yuji91 <yuji91.1227@gmail.com>
2021-09-09 18:01:14 +0900

    rails generate controller users new create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/create'
   get 'top_pages/index'
   get 'top_pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
